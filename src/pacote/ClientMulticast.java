97a479aee576f48e343168e1c1426225940af87d
João Ricardo Leitão Barros <56694154+Trutzys@users.noreply.github.com>
2024-04-01 09:27:45 -0300

    Add files via upload

--- src/pacote/ClientMulticast.java
+++ src/pacote/ClientMulticast.java
@@ -9,15 +9,17 @@ import java.util.Scanner;
 
 public class ClientMulticast implements Runnable{
    private int var;
-   public ClientMulticast(int entrada) {
+   private int action;
+   public ClientMulticast(int entrada, int acao) {
 	   var = entrada;
+	   action = acao;
    }
    
    public void run() {
 	   try {
 		   String msg = " ";	      
 		      MulticastSocket socket=new MulticastSocket(var);
-		      
+		      byte[] buffer = new byte[1024];
 		      InetAddress ia =InetAddress.getByName("230.0.0.0");
 		      InetSocketAddress grupo = new InetSocketAddress(ia , var);
 		      NetworkInterface ni = NetworkInterface.getByInetAddress(ia);
@@ -29,18 +31,43 @@ public class ClientMulticast implements Runnable{
 		      Scanner name = new Scanner(System.in);
 		      nome = name.nextLine();
 		      
-		      while(!msg.contains("Servidor Encerrado!")){
-		         System.out.println("[ " + nome + " ] Conectando ao servidor");
-		         byte[] buffer = new byte[1024];
-		         
-		         DatagramPacket packet=new DatagramPacket(buffer,buffer.length);
-		         socket.receive(packet);
-		         
-		         msg=new String(packet.getData());
-		         System.out.println("[ " + nome + " ] " + msg);
-		       
+		      if(action == 1) {
+		    	  System.out.println("[ " + nome + " ] Conectando ao servidor");
+			         
+			      DatagramPacket packet=new DatagramPacket(buffer,buffer.length);
+			      socket.receive(packet);
+			         
+			      msg=new String(packet.getData());
+			      System.out.println("[ " + nome + " ] " + msg);
+			      
+		      }else if(action == 2) {
+		    	  String escolha;
+		    	  String tempo;
+		    	  System.out.println("[ " + nome + " ] Conectando ao servidor");				         				         
+				         
+		    	  DatagramPacket packet=new DatagramPacket(buffer,buffer.length);			      
+		    	  socket.receive(packet);
+		    		  
+		    	  msg=new String(packet.getData());				      
+		    	  System.out.println("[ " + nome + " ] " + msg);				         				      
+		    	  System.out.println("[ " + nome + " ] Escolha o carro desejado para alugar:\n");				      				      
+		    	  Scanner choice = new Scanner(System.in);				      
+		    	  escolha = choice.nextLine();				      
+		    	  System.out.println("[ " + nome + " ] Carro escolhido: " + escolha + ":\n");
+		    	  
+		      }else if(action == 3) {				         
+		    	  String escolha;
+		    	  System.out.println("[ " + nome + " ] Escolha o carro desejado para alugar:\n");				      				      
+		    	  Scanner choice = new Scanner(System.in);				      
+		    	  escolha = choice.nextLine();				      
+		    	  System.out.println("[ " + nome + " ] Carro escolhido: " + escolha + ":\n");
+				  buffer = escolha.getBytes();
+				  
+		    	  DatagramPacket packet2 = new DatagramPacket(buffer, buffer.length,ia, 4323);				      			    		 
+		    	  socket.send(packet2);				         
 		      }
-		      System.out.println("[ " + nome + " ] Conexao com o servidor encerrada!");
+		      
+			  System.out.println("[ " + nome + " ] Conexao com o servidor encerrada!");
 		      socket.leaveGroup(grupo, ni);
 		      socket.close();
 	   }catch(Exception e) {
@@ -52,24 +79,21 @@ public class ClientMulticast implements Runnable{
    public static void main(String[] args) throws IOException {	   
 	       
 	      System.out.println("Escolha o topico desejado:");
-	      System.out.println("1 -Anuncios | 2 - Alugar carro | 3 - Devolver carro");
+	      System.out.println("1 -Anuncios | 2 - Alugar carro | 3 - Devolver carro | 4 - Encerrar");
 	      int entrada;
 	      Scanner entry = new Scanner(System.in);
 	      entrada = entry.nextInt();
-	      if(entrada == 1) {
-	    	  ClientMulticast client1 = new ClientMulticast(4321);
-	    	  new Thread(client1).start();
-		      
-	      }else if(entrada == 2) {
-	    	  ClientMulticast client2 = new ClientMulticast(4322);
-	    	  new Thread(client2).start();
-		      
-	      }else if(entrada == 3) {
-
-	      }
-	      
-	   
-   }
-
-  
+		  if(entrada == 1) {
+			  ClientMulticast client1 = new ClientMulticast(4321, entrada);
+		      new Thread(client1).start();
+			      
+		  }else if(entrada == 2) {
+		      ClientMulticast client2 = new ClientMulticast(4322, entrada);
+		      new Thread(client2).start();
+			      
+		  }else if(entrada == 3) {
+		      ClientMulticast client3 = new ClientMulticast(4323, entrada);
+		      new Thread(client3).start();
+		  }
+   	}
 }

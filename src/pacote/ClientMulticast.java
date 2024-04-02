72c5f6916ba029550ea8fc2c13baa5d2519b72c9
DjdogeGamer <gustavohco34@gmail.com>
2024-04-02 17:47:54 -0300

    FEAT: Implementação inicial Criar Modelos e Lançar Anuncios

--- src/pacote/ClientMulticast.java
+++ src/pacote/ClientMulticast.java
@@ -1,4 +1,5 @@
 package pacote;
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
@@ -7,93 +8,118 @@ import java.net.MulticastSocket;
 import java.net.NetworkInterface;
 import java.util.Scanner;
 
-public class ClientMulticast implements Runnable{
-   private int var;
-   private int action;
-   public ClientMulticast(int entrada, int acao) {
-	   var = entrada;
-	   action = acao;
-   }
-   
-   public void run() {
-	   try {
-		   String msg = " ";	      
-		      MulticastSocket socket=new MulticastSocket(var);
-		      byte[] buffer = new byte[1024];
-		      InetAddress ia =InetAddress.getByName("230.0.0.0");
-		      InetSocketAddress grupo = new InetSocketAddress(ia , var);
-		      NetworkInterface ni = NetworkInterface.getByInetAddress(ia);
-		      
-		      socket.joinGroup(grupo,ni);
-		      
-		      System.out.println("Digite o seu nome: ");
-		      String nome;
-		      Scanner name = new Scanner(System.in);
-		      nome = name.nextLine();
-		      
-		      if(action == 1) {
-		    	  System.out.println("[ " + nome + " ] Conectando ao servidor");
-			         
-			      DatagramPacket packet=new DatagramPacket(buffer,buffer.length);
-			      socket.receive(packet);
-			         
-			      msg=new String(packet.getData());
-			      System.out.println("[ " + nome + " ] " + msg);
-			      
-		      }else if(action == 2) {
-		    	  String escolha;
-		    	  String tempo;
-		    	  System.out.println("[ " + nome + " ] Conectando ao servidor");				         				         
-				         
-		    	  DatagramPacket packet=new DatagramPacket(buffer,buffer.length);			      
-		    	  socket.receive(packet);
-		    		  
-		    	  msg=new String(packet.getData());				      
-		    	  System.out.println("[ " + nome + " ] " + msg);				         				      
-		    	  System.out.println("[ " + nome + " ] Escolha o carro desejado para alugar:\n");				      				      
-		    	  Scanner choice = new Scanner(System.in);				      
-		    	  escolha = choice.nextLine();				      
-		    	  System.out.println("[ " + nome + " ] Carro escolhido: " + escolha + ":\n");
-		    	  
-		      }else if(action == 3) {				         
-		    	  String escolha;
-		    	  System.out.println("[ " + nome + " ] Escolha o carro desejado para alugar:\n");				      				      
-		    	  Scanner choice = new Scanner(System.in);				      
-		    	  escolha = choice.nextLine();				      
-		    	  System.out.println("[ " + nome + " ] Carro escolhido: " + escolha + ":\n");
-				  buffer = escolha.getBytes();
-				  
-		    	  DatagramPacket packet2 = new DatagramPacket(buffer, buffer.length,ia, 4323);				      			    		 
-		    	  socket.send(packet2);				         
-		      }
-		      
-			  System.out.println("[ " + nome + " ] Conexao com o servidor encerrada!");
-		      socket.leaveGroup(grupo, ni);
-		      socket.close();
-	   }catch(Exception e) {
-		   System.out.println("Erro");
-		   return;
-	   }
-   }
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class ClientMulticast implements Runnable {
 	
-   public static void main(String[] args) throws IOException {	   
-	       
-	      System.out.println("Escolha o topico desejado:");
-	      System.out.println("1 -Anuncios | 2 - Alugar carro | 3 - Devolver carro | 4 - Encerrar");
-	      int entrada;
-	      Scanner entry = new Scanner(System.in);
-	      entrada = entry.nextInt();
-		  if(entrada == 1) {
-			  ClientMulticast client1 = new ClientMulticast(4321, entrada);
-		      new Thread(client1).start();
-			      
-		  }else if(entrada == 2) {
-		      ClientMulticast client2 = new ClientMulticast(4322, entrada);
-		      new Thread(client2).start();
-			      
-		  }else if(entrada == 3) {
-		      ClientMulticast client3 = new ClientMulticast(4323, entrada);
-		      new Thread(client3).start();
-		  }
-   	}
+	private String nome;
+	private int var;
+	private int action;
+
+	public ClientMulticast(int entrada, int acao) {
+		var = entrada;
+		action = acao;
+	}
+
+	public void run() {
+		try {
+			// Region : Socket Config
+			String msgServidor = " ";
+			MulticastSocket socket = new MulticastSocket(var);
+			byte[] buffer = new byte[1024];
+			InetAddress ia = InetAddress.getByName("230.0.0.0");
+			InetSocketAddress grupo = new InetSocketAddress(ia, var);
+			NetworkInterface ni = NetworkInterface.getByInetAddress(ia);
+			socket.joinGroup(grupo, ni);
+			
+			// Region : Start
+			
+			System.out.println("Digite o seu nome: ");
+			Scanner scan = new Scanner(System.in);
+			
+			this.nome = scan.nextLine();
+
+			if (this.action == 1) {
+				System.out.println("[ " + nome + " ] Conectando ao servidor");
+
+				// Region: Tratamento do Pacote
+				
+				DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+				socket.receive(packet);
+
+				msgServidor = new String(packet.getData());
+				
+				JSONArray jsonServidor = new JSONArray(msgServidor);
+		
+	            for (Object obj : jsonServidor) {
+	            	JSONObject jsonObject = (JSONObject) obj;		
+	            	if (jsonObject.has("mensagem")) {	            		
+	            		System.out.println("MENSAGEM PARA ANÚNCIOS : " + jsonObject.getString("mensagem"));
+	            	} else if (jsonObject.has("modelo")) {		
+	            		System.out.println(" - " + jsonObject.getString("modelo"));
+	            	}
+	            }
+
+			} else if (this.action == 2) {
+				String escolha;
+				String modeloNome;
+				String tempo;
+				System.out.println("[ " + nome + " ] Conectando ao servidor");
+
+				DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+				socket.receive(packet);
+
+				msgServidor = new String(packet.getData());
+				System.out.println("[ " + nome + " ] " + msgServidor);
+				System.out.println("[ " + nome + " ] Escolha o carro desejado para alugar:\n");
+				Scanner choice = new Scanner(System.in);
+				escolha = choice.nextLine();
+				System.out.println("[ " + nome + " ] Carro escolhido: " + escolha + ":\n");
+
+			} else if (this.action == 3) { // TODO
+				String escolha;
+				System.out.println("[ " + nome + " ] Escolha o carro desejado para alugar:\n");
+				Scanner choice = new Scanner(System.in);
+				escolha = choice.nextLine();
+				System.out.println("[ " + nome + " ] Carro escolhido: " + escolha + ":\n");
+				buffer = escolha.getBytes();
+
+				DatagramPacket packet2 = new DatagramPacket(buffer, buffer.length, ia, 4323);
+				socket.send(packet2);
+			} else if (this.action == 4) {				
+				System.out.println("[ " + nome + " ] Conexao com o servidor encerrada!");
+				socket.leaveGroup(grupo, ni);
+				socket.close();
+			} 
+			main(null);
+
+		} catch (Exception e) {
+			System.out.println(e);
+			return;
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("Escolha o topico desejado:");
+		System.out.println("1 - Anuncios | 2 - Alugar carro | 3 - Devolver carro | 4 - Encerrar");
+		int entrada;
+		Scanner entry = new Scanner(System.in);
+		entrada = entry.nextInt();
+		if (entrada == 1) {
+			ClientMulticast client1 = new ClientMulticast(4321, entrada);
+			client1.action = entrada;
+			new Thread(client1).start();
+
+		} else if (entrada == 2) {
+			ClientMulticast client2 = new ClientMulticast(4322, entrada);
+			client2.action = entrada;
+			new Thread(client2).start();
+
+		} else if (entrada == 3) {
+			ClientMulticast client3 = new ClientMulticast(4323, entrada);
+			client3.action = entrada;
+			new Thread(client3).start();
+		}
+	}
 }
--- src/pacote/ServerMulticast.java
+++ src/pacote/ServerMulticast.java
@@ -1,8 +1,9 @@
 package pacote;
+
 import java.io.IOException;
-import java.time.LocalDate; 
-import java.time.LocalDateTime; 
-import java.time.LocalTime; 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -10,50 +11,105 @@ import java.net.InetSocketAddress;
 import java.net.MulticastSocket;
 import java.net.NetworkInterface;
 import java.util.Scanner;
+import org.json.*;
 
 public class ServerMulticast {
-   public static void main(String[] args) throws IOException {
-	   String mensagem = " ";
-	   byte[] envio = new byte[1024];
-	   LocalDateTime date1 = LocalDateTime.now(); 
-	   Scanner sc = new Scanner(System.in);
-	   
-	   MulticastSocket socket = new MulticastSocket();
-	   InetAddress ia = InetAddress.getByName("230.0.0.0");
-	   
-	   while(!mensagem.equals("Servidor Encerrado!")){
-		   int acao = 0;
-		   Scanner action = new Scanner(System.in);
-		   System.out.print("[Servidor] Escolha a ação a ser tomada: \n "
-		   				  + "1 - Enviar Anuncios | 2 - Emprestimo | 3 - Devolução | 4 - Encerrar Serviço\n");
-		   acao = action.nextInt();
-		   if(acao == 1) {
-			   System.out.print("[Servidor] Digite a mensagem para anuncios:\n");
-			   mensagem = sc.nextLine();
-			   mensagem = "[" + date1 + "]" + "\n\n              Anuncios!\n\n" + mensagem + "\n\n";
-			   envio = mensagem.getBytes();
-			   
-			   DatagramPacket pacote = new DatagramPacket(envio, envio.length,ia, 4321);
-			   socket.send(pacote);
-			   
-		   }else if(acao == 2) {
-			   System.out.print("[Servidor] Digite a lista de carros disponiveis:\n");
-			   mensagem = sc.nextLine();
-			   mensagem = "[" + date1 + "]" + "\nLista de carros disponiveis para aluguel!\n" + mensagem;
-			   envio = mensagem.getBytes();
-			   
-			   DatagramPacket pacote = new DatagramPacket(envio, envio.length,ia, 4322);
-			   socket.send(pacote);			   
-			   
-		   }else if(acao == 3) {
-			   
-			   
-		   }else if(acao == 4) {
-			   System.out.print("[Servidor] Encerrando serviço de atendimento.\n");
-			   mensagem = "Servidor Encerrado!";
-			   socket.close();  
-		   }	   
-		   
-	   }
-   }
+	
+	public static void main(String[] args) throws IOException {
+		
+		String mensagem = " ";
+		byte[] envio = new byte[1024];
+		LocalDateTime date1 = LocalDateTime.now();
+		Scanner sc = new Scanner(System.in);
+		
+		// Objeto carro : DO LATER
+		String modelo;
+		String nome;
+		String disponivel;
+		String tempo;
+		
+		// Region : Var
+		
+		JSONArray listModelos = new JSONArray();
+
+		MulticastSocket socket = new MulticastSocket();
+		InetAddress ia = InetAddress.getByName("230.0.0.0");
+
+		while (!mensagem.equals("Servidor Encerrado!")) {
+			
+			int acao = 0;
+			
+			Scanner action = new Scanner(System.in);
+			System.out.print("[Servidor] Escolha a ação a ser tomada: \n "
+					+ "1 - Enviar Anuncios | 2 - Emprestimo | 3 - Devolução | 4 - Encerrar Serviço\n");
+			
+			acao = action.nextInt();
+			
+			if (acao == 1) {
+				System.out.print("[Servidor] Digite a mensagem para anuncios:\n");
+				mensagem = sc.nextLine();
+				
+				JSONObject msgJSON = new JSONObject();
+				msgJSON.put("mensagem", mensagem);
+				listModelos.put(msgJSON);
+				
+				mensagem = "[" + date1 + "]" + "\n\n ---- Anuncios! ---- \n\n" + mensagem + "\n\n";
+	            for (Object obj : listModelos) {
+	            	JSONObject jsonObject = (JSONObject) obj;		
+	            	if (jsonObject.has("modelo")) {		
+	            		System.out.println(" - " + jsonObject.getString("modelo"));
+	            	}
+	            }
+	            
+	            byte[] jsonEnvio = listModelos.toString().getBytes();
+				envio = jsonEnvio;
+				DatagramPacket pacote = new DatagramPacket(envio, envio.length, ia, 4321);
+				socket.send(pacote);
+
+			} else if (acao == 2) {
+				System.out.print("[Servidor] Digite a lista de carros disponiveis, FIM para terminar :\n");
+				
+				// Region : Registrando novos modelos
+				
+				while (true) {
+					JSONObject novoModelo = new JSONObject();
+					System.out.print("[Servidor] Digite o modelo do carro :\n");
+					mensagem = sc.nextLine();
+					
+					if (mensagem.equals("FIM")) {
+						break;
+					}
+					
+					novoModelo.put("modelo", mensagem);
+					listModelos.put(novoModelo);
+					
+				}
+				
+				mensagem = "[" + date1 + "]" + "\nLista de carros disponiveis para aluguel!\n" + mensagem;
+				
+				// Region : Listando Todos os Modelos
+				
+	            for (Object obj : listModelos) {
+	                JSONObject jsonObject = (JSONObject) obj;
+	                System.out.println(" - " + jsonObject.getString("modelo"));
+	            }
+	            
+	            // Region : Tratando e enviando novos modelos
+	            
+	            byte[] jsonEnvio = listModelos.toString().getBytes();
+				envio = jsonEnvio;
+				
+				DatagramPacket pacote = new DatagramPacket(envio, envio.length, ia, 4322);
+				socket.send(pacote);
+
+			} else if (acao == 3) {
+
+			} else if (acao == 4) {
+				System.out.print("[Servidor] Encerrando serviço de atendimento.\n");
+				mensagem = "Servidor Encerrado!";
+				socket.close();
+			}
+
+		}
+	}
 }
